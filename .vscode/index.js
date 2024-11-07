2c66d8e4bd6d8739f648bb37597d511e25291622
shreya <114135381+SShreyagaur@users.noreply.github.com>
2024-11-07 16:54:05 +0530

    code

--- .vscode/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('timetableForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent page from refreshing
-    
-    const branch = document.getElementById('branch').value;
-    const section = document.getElementById('section').value;
-
-    // Redirect to the timetable page, passing the values as query parameters
-    window.location.href = `timetable.html?branch=${branch}&section=${section}`;
-});
